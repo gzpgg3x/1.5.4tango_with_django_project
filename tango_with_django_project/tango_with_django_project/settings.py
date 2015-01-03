7883846c576aca6f634bd57d9f0572ca19574d69
gzpgg3x <gzpgg3x@yahoo.com>
2015-01-02 21:32:58 -0500

    4.6. Exercises
    4.6. Exercises

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -8,6 +8,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -101,5 +103,7 @@ STATICFILES_DIRS = (
     STATIC_PATH,
 )
 
-
 # STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media') # Absolute path to the media directory
