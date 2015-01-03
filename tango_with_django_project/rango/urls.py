2377bcdecfa1ff0b07bb631de13db4bcfed6b36c
gzpgg3x <gzpgg3x@yahoo.com>
2015-01-03 12:21:16 -0500

    before6.4. ExercisesBulletOne
    before6.4. ExercisesBulletOne

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -3,5 +3,7 @@ from rango import views
 
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
+        # url(r'^page/', views.page, name='page'),
+        url(r'^category/(?P<category_name_url>\w+)/$', views.category, name='category'),  # New!           
         url(r'^about/', views.about, name='about'),
     )
